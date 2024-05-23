dbec52223548174a6b692de79f47420b384b61dc
shahriar pahlevansadegh <apachish@gmail.com>
2024-05-23 16:36:37 +0330

    admin

--- app/Http/Controllers/TelegramAdminController.php
+++ app/Http/Controllers/TelegramAdminController.php
@@ -40,9 +40,9 @@ class TelegramAdminController extends Controller
 
         $user_id = data_get($update, $type . '.from.id');
 
-        $access = $bot->accessBot->where("user_id",$user_id)->where("type","admin");
+        $access = $bot->accessBot->where("user_id", $user_id)->where("type", "admin");
 
-        if($access ==  null) return false;
+        if ($access == null) return false;
         $user_telegram = UserTelegram::find($user_id);
         if ($user_telegram == null) {
             $user_telegram = UserTelegram::create([
@@ -56,18 +56,24 @@ class TelegramAdminController extends Controller
             ]);
         }
         $chatId = $update[$type]['from']['id']; // چت‌آیدی کاربر
-
-        $this->menu($telegram,$chatId);
+        logger("chatid" . $chatId);
+        $this->menu($telegram, $chatId);
 
 
     }
 
-    public function menu($telegram,$chatId)
+    public function menu($telegram, $chatId)
     {
-                $keyboard = [
-            ["text"=>'\ud83e\uddd1\u200d\ud83e\udd1d\u200d\ud83e\uddd1 لیست کاربران',"callback_data"=>"list"],
-            ["text"=>'\ud83e\uddd1\u200d\ud83e\udd1d\u200d\ud83e\uddd1 لیست کاربران منتظر تایید',"callback_data"=>"pending"],
-            ['text'=>"\u21a9\ufe0f بازگشت به ","callback_data"=>"return"]
+        $keyboard = [
+            [
+                ["text" => '\ud83e\uddd1\u200d\ud83e\udd1d\u200d\ud83e\uddd1 لیست کاربران', "callback_data" => "list"]
+            ],
+            [
+                ["text" => '\ud83e\uddd1\u200d\ud83e\udd1d\u200d\ud83e\uddd1 لیست کاربران منتظر تایید', "callback_data" => "pending"]
+            ],
+            [
+                ['text' => "\u21a9\ufe0f بازگشت به ", "callback_data" => "return"]
+            ]
         ];
         $reply_markup = Keyboard::make([
             'inline_keyboard' => $keyboard,
