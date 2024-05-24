d7e374e0e83bdab20a9273e23eeafa80e6e17f98
shahriar pahlevansadegh <apachish@gmail.com>
2024-05-25 01:09:10 +0330

    update

--- app/Http/Controllers/TelegramAdminController.php
+++ app/Http/Controllers/TelegramAdminController.php
@@ -121,8 +121,8 @@ class TelegramAdminController extends Controller
                     $text .= "\n\n" . $user->mobile;
 
                     $keyboard[$i] = [
-                        ['text' => "✅ $text ", 'callback_data' => 'confirm_' . $user->id],
-                        ['text' => "❌ $text", 'callback_data' => 'reject_' . $user->id]
+                        ['text' => "✅ \n\n $text ", 'callback_data' => 'confirm_' . $user->id],
+                        ['text' => "❌ \n\n $text", 'callback_data' => 'reject_' . $user->id]
                     ];
                 });
                 logger("keyboard", [$keyboard]);
