b5503f124f8dcf5280beece629ff6dbf4e6cd07c
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-01 01:05:24 +0330

    update

--- app/Services/TextServices.php
+++ app/Services/TextServices.php
@@ -564,7 +564,7 @@ class TextServices
                 logger("response",[$response]);
                 if(isset($response['message_id']))
                 {
-                    $this->user->menu[$this->bot->title] = $response['message_id'];
+                    $this->user->menu = $response['message_id'];
                     $this->user->update();
                 }
                 cache()->set("keyword_menu" . $this->getKeyCache() . $this->user->id, true);
