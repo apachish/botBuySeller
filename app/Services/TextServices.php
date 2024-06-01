89b9e10d18d02d83b87c76a0a8ccf7a60450722a
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-01 12:09:38 +0330

    up

--- app/Services/TextServices.php
+++ app/Services/TextServices.php
@@ -349,6 +349,9 @@ class TextServices
        * check message
        */
         logger("check message",[$this->checkText()]);
+        logger("cache",[$this->message_cache]);
+        cache()->forget($this->key_cache.$this->user_id);
+        logger("cache",[$this->message_cache]);
 
         if (!$this->checkText())
             return $this->telegram->sendMessage(['chat_id' => $this->user_id, 'text' => 'متن نا معتبر می باشد']);
