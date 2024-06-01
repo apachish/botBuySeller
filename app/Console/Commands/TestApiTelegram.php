3b21c9e06df644b6ba3bf10a578a6a59c913fb24
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-01 08:09:53 +0330

    updtaE

--- app/Console/Commands/TestApiTelegram.php
+++ app/Console/Commands/TestApiTelegram.php
@@ -3,8 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Models\Bot;
+use App\Models\BotMenuUser;
+use App\Models\UserTelegram;
 use App\Services\TelegramServices;
 use Illuminate\Console\Command;
+use Telegram\Bot\Api;
 
 class TestApiTelegram extends Command
 {
@@ -30,24 +33,45 @@ class TestApiTelegram extends Command
         $token = $this->ask('What is  token bot?');
         $user_id = $this->ask('What is  user_id?');
         $menu_id = $this->ask('What is  menu_id?');
-        $keyword_colleague = [
-            [
-                ['text' => "\xF0\x9F\x91\xA5	معرفی مشتری"],
-                ['text' => "\xF0\x9F\x93\x8B	لیست همکاران"],
-            ],
-            [
-                ['text' => "\xF0\x9F\x93\x88	معاملات باز"]
-            ],
-            [
-                ['text' => "\xF0\x9F\x93\x9A	قوانین"],
-                ['text' => "راهنما \xE2\x81\x89"]
-            ], [
-                ['text' => "\xE2\x9C\x8C	فعال سازی دو مرحله ای"],
-                ['text' => "\xE2\x9D\x8C	غیر فعال فوری"],
+        if(!$menu_id) {
+            $telegram = new Api($token);
+            $keyword_customer = [
+                [
+                    ['text' => "\xF0\x9F\x93\x9A	قوانین"],
+                    ['text' => "راهنما \xE2\x81\x89"]
+                ], [
+                    ['text' => "\xE2\x9C\x8C	فعال سازی دو مرحله ای"],
+                    ['text' => "\xE2\x9D\x8C	غیر فعال فوری"],
 
-            ]];
-        $telegram_services = new TelegramServices($token);
-        $d = $telegram_services->editCustomKeyboard($user_id, $menu_id, "تغییر منو", $keyword_colleague);
-        dd($d);
+                ]];
+            $user = UserTelegram::where("id", $user_id)->first();
+            $response = TelegramServices::menu($telegram, $keyword_customer, $user, "تست");
+            logger("response", [$response]);
+            if (isset($response['message_id'])) {
+                $this->info($response['message_id']);
+                BotMenuUser::updateOrCreate(["user_id" => $this->user_id, "bot_id" => $this->bot->id], ["menu_id" => $response['message_id']]);
+                $this->user->update();
+            }
+        }else {
+            $keyword_colleague = [
+                [
+                    ['text' => "\xF0\x9F\x91\xA5	معرفی مشتری"],
+                    ['text' => "\xF0\x9F\x93\x8B	لیست همکاران"],
+                ],
+                [
+                    ['text' => "\xF0\x9F\x93\x88	معاملات باز"]
+                ],
+                [
+                    ['text' => "\xF0\x9F\x93\x9A	قوانین"],
+                    ['text' => "راهنما \xE2\x81\x89"]
+                ], [
+                    ['text' => "\xE2\x9C\x8C	فعال سازی دو مرحله ای"],
+                    ['text' => "\xE2\x9D\x8C	غیر فعال فوری"],
+
+                ]];
+            $telegram_services = new TelegramServices($token);
+            $d = $telegram_services->editCustomKeyboard($user_id, $menu_id, "تغییر منو", $keyword_colleague);
+            dd($d);
+        }
     }
 }
