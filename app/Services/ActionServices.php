f20d4b1d6113ff4236f6c3f7ddee1a959c05d928
apachish <apachish@gmail.com>
2024-06-02 18:40:55 +0330

    update

--- app/Services/ActionServices.php
+++ app/Services/ActionServices.php
@@ -97,7 +97,7 @@ class ActionServices extends TextServices
     {
         $this->getUser()->fullName = $this->message;
         $this->getUser()->update();
-        if($this->getUser()->mobile) {
+        if(!$this->getUser()->mobile) {
             $text = "ممنون شماره خود را به اشتراک بگذارید";
             $this->telegram_services->sendRequestContactButton($this->getUserId(), $text);
         }
