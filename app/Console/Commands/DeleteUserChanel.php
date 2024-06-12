1a5b0a8823625457bce8e6dfe6305273a23ab4af
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-12 17:34:52 +0330

    update

---
+++ app/Console/Commands/DeleteUserChanel.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Bot;
+use App\Models\Transfer;
+use App\Models\UserTelegram;
+use App\Services\TelegramServices;
+use Illuminate\Console\Command;
+
+class DeleteMessageChanel extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-user-chanel';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $bot = Bot::where("title","botUser")->first();
+
+        if($bot) {
+            logger("delete Message");
+            // تعیین تاریخ مورد نظر
+            $targetDate = now()->subDay(1); // تاریخ مورد نظر برای حذف پیام‌ها (فرمت YYYY-MM-DD)
+            $telegram = new TelegramServices($bot->token);
+            $users = UserTelegram::all();
+            foreach ($users as $user)
+            {
+                $this->telegram_services->kickChatMember($this->bot->chanel_id, $user->id);
+            }
+            logger("end delete Message");
+        }
+
+    }
+
+// تابع برای حذف پیام
+}
