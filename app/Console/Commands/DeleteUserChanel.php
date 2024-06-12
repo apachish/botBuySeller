1930d1b80df57a2600fe7c2a5c7d6a94a53f08e0
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-12 17:39:02 +0330

    update

--- app/Console/Commands/DeleteUserChanel.php
+++ app/Console/Commands/DeleteUserChanel.php
@@ -40,6 +40,7 @@ class DeleteUserChanel extends Command
             foreach ($users as $user)
             {
                 $telegram->kickUserFromChannel($bot->chanel_id, $user->id);
+                $telegram->kickChatMember($bot->chanel_id, $user->id);
             }
             logger("end delete Message");
         }
