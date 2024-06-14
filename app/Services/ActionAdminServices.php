02b626562df3014cb47f5f2d17ebf39a548f8aaa
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-14 15:41:59 +0330

    update

--- app/Services/ActionAdminServices.php
+++ app/Services/ActionAdminServices.php
@@ -217,7 +217,7 @@ class ActionAdminServices extends TextServices
             if ($user_con) {
                 logger("link",[
                     $this->bot,
-                    'chat_id' => $this->bot->chanel_id,
+                    'chat_id' => (int)$this->bot->chanel_id,
                     'expire_date' => time() + 3600, // لینک به مدت 24 ساعت معتبر است
                     'name' => Str::slug($user_con->fullName,"_"),
                     'member_limit' => 1, // تعداد اعضای جدیدی که با این لینک می‌توانند بپیوندند
