b95395a3a9168e205a81f19e83fa53b60dcde3e9
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-14 14:54:31 +0330

    update

--- app/Services/ActionAdminServices.php
+++ app/Services/ActionAdminServices.php
@@ -220,7 +220,7 @@ class ActionAdminServices extends TextServices
                     'member_limit' => 1, // تعداد اعضای جدیدی که با این لینک می‌توانند بپیوندند
                 ]);
                 $response = $this->telegram->createChatInviteLink([
-                    'chat_id' => $this->bot->chanel_id,
+                    'chat_id' => $this->bot_user->chanel_id,
                     'expire_date' => time() + 3600, // لینک به مدت 24 ساعت معتبر است
                     'member_limit' => 1, // تعداد اعضای جدیدی که با این لینک می‌توانند بپیوندند
                 ]);
