058e584bf298990145d3f0318f8c511ef32a97b7
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-20 00:47:06 +0330

    update

--- app/Utilities/Helpers.php
+++ app/Utilities/Helpers.php
@@ -42,29 +42,24 @@ if (!function_exists('isValidTime')) {
 }
 if (!function_exists('cleanInput')) {
     function cleanInput($input) {
-        $list_type = [  "خفپ","ففپ","خفم","ففم","خفن","خفش","ففش","ففن","فف","خف","خش","فش","خن","فن","خم","فم","خپ","فپ","خ","ف",
-            "خ ف پ","ف ف پ","خ ف م","ف ف م","خ ف ن","خ ف ش","ف ف ش","ف ف ن","ف ف","خ ف","خ ش","ف ش","خ ن","ف ن","خ م","ف م","خ پ","ف پ"
-
-        ];
-        $input =  rtrim($input);
-
-        $im = implode("|",$list_type);
-        $pattern = "/^([0-9]{3}|[0-9]{5})($im)([1-3]?)(.*:.*)?$/u";
+        // استفاده از یک الگو برای جدا کردن بخش‌ها
+        $pattern = '/^(\d+)\s*( خفن|خفش|ففش|ففن|خفپ|ففپ|خفم|ففم|فف|خف|فپ|خپ|فم|خم|خش|فش|خن|فن|خ|ف)\s*(\d?)(:\s*(.*))?$/u';
 
         if (preg_match($pattern, $input, $matches)) {
+            // حذف فضای خالی از بخش‌های مورد نیاز
+            $number = preg_replace('/\s+/', '', $matches[1]);
+            $letters = preg_replace('/\s+/', '', $matches[2]);
+            $optional_number = preg_replace('/\s+/', '', $matches[3]);
+            $comment = isset($matches[5]) ? $matches[5] : '';
 
-            logger("match",[$matches]);
-            $message = explode(":",$input);
-            $word = str_ireplace(" ","",$message[0]);
-
-            if(isset($message[1]))
-                $cleanedInput = $word.":".$message[1];
-            else
-                $cleanedInput = $word;
-            return  $cleanedInput;
-        }else{
-            return $input;
+            // ساختن رشته نهایی
+            $cleanedInput = $number . $letters . $optional_number;
+            if (!empty($comment)) {
+                $cleanedInput .= ':' . $comment;
+            }
+            return $cleanedInput;
         }
+        return $input; // اگر الگو تطابق نداشت، همان ورودی را برگردانید
     }
 }
 if (!function_exists('getPriceFormat')) {
