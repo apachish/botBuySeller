7be159883e73f796d43f785b910ee42f56cc37bd
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-21 23:06:29 +0330

    update

--- database/migrations/2024_06_21_225422_create_user_trade_access_table.php
+++ database/migrations/2024_06_21_225422_create_user_trade_access_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('user_telegram');
-            $table->id();
             $table->unsignedBigInteger('user_trade_id');
             $table->foreign('user_trade_id')->references('id')->on('user_telegram');
             $table->integer("limit_access");
