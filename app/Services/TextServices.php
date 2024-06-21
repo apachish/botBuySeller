5c006cb5becf1b73622deef0ce96bd4fcba6c955
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-22 01:44:05 +0330

    up

--- app/Services/TextServices.php
+++ app/Services/TextServices.php
@@ -713,7 +713,7 @@ class TextServices
                     if($this->getUser()->memberShip) {
                         $message = "تاریخ پایان اشتراک شما:";
                         $message .= "\n\n";
-                        $message .= toJalali($this->getUser()->memberShip->date);
+                        $message .= toJalali($this->getUser()->memberShip->date,"y/d/m");
                     }else{
                         $message = " اشتراک ندارید";
                     }
