8647ae98fdeb8d970def94319447d776612a0283
apachish <apachish@gmail.com>
2024-06-22 09:51:01 +0330

    up

--- app/Http/Controllers/TelegramCustomerController.php
+++ app/Http/Controllers/TelegramCustomerController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 
-
 use App\Services\SupportServices;
 
 class TelegramCustomerController extends Controller
@@ -18,13 +17,26 @@ class TelegramCustomerController extends Controller
             $key_cache = "text_user_customer";
             $text_services->setMessage();
             $text_services->setUser();
-            logger("user",[$text_services->getUser()]);
-            if($text_services->getUser() == null) return false;
+            logger("user", [$text_services->getUser()]);
+            if ($text_services->getUser() == null) return false;
+
+            if ($text_services->getMessage() == "/start") {
+                $message = "با تشکر از ثبت نام در ربات مشتریان";
+                $message .= "\n\n";
+                $message .= "
+از این پس فاکتور هر معامله ای که توسط زیر مجموعه هایتان انجام شود اینجا برایتان ارسال میگردد";
+                $message .= "\n\n";
+                $message .= "
+در نظر داشته باشید اسم طرف معامله مشتریانتان فقط برای شما قابل روئیت میباشد";
+
+                    $text_services->telegram_services->sendMessage($text_services->getUserId(), $message);
+            } else {
+
+            }
 
 
-
-        }catch (\Exception $exception){
-            logger("get error",[
+        } catch (\Exception $exception) {
+            logger("get error", [
                 $exception->getMessage(),
                 $exception->getLine(),
                 $exception->getCode(),
