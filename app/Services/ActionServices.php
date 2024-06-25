9589fa954be0f7790e830aee8985e2063abb3ef3
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-26 00:03:50 +0330

    up

--- app/Services/ActionServices.php
+++ app/Services/ActionServices.php
@@ -540,15 +540,23 @@ class ActionServices extends TextServices
         }
 
         $seller_head = $seller->customer;
+        logger("1",[$seller_head,$seller->customerUser]);
         $seller_customer = $seller->customerUser?$seller->customerUser->pluck("id")->toArray():[];
+        logger("2",[$seller_customer]);
         $seller_ids[] = $seller->id;
+        logger("23",[$seller_ids]);
+
         if ($seller_head)
             $seller_ids[] = $seller_head->id;
         if ($seller_customer)
             $seller_ids = array_merge($seller_ids, $seller_customer);
 
         $buyer_head = $buyer->customer;
+        logger("12",[$buyer_head,$buyer->customerUser]);
+
         $buyer_customer = $buyer->customerUser?$buyer->customerUser->pluck("id")->toArray():[];
+        logger("22",[$buyer_customer]);
+
         $buyer_ids[] = $buyer->id;
         if ($buyer_head)
             $buyer_ids[] = $buyer_head->id;
