c2a6bb0324b4e81d9405b4b90d1ce1e3b639f6f8
shahriar pahlevansadegh <apachish@gmail.com>
2024-06-26 03:31:13 +0330

    update

--- app/Services/ActionServices.php
+++ app/Services/ActionServices.php
@@ -883,14 +883,14 @@ class ActionServices extends TextServices
             foreach ($me as $req) {
                 $request_transfer[] = $req;
                 if (data_get($customer, 'id') == data_get($req, 'transfer.user_id')) {
-                    $req->type_label = data_get($req, "type");
+                    $req->type_label = data_get($req, "type")== "buy" ? "sell" : "buy";
                     $req->said = data_get($req, "userRequest.fullName");
                     $req->color = $req->type_label == "sell" ? "#ef4444" : "dodgerblue";
                     logger("1", [$req, data_get($req, "userRequest"), data_get($req, "userRequest.fullName"), data_get($req, "transfer"), data_get($req, "transfer.user")]);
 
                 } else {
 
-                    $req->type_label = data_get($req, "type") == "buy" ? "sell" : "buy";
+                    $req->type_label = data_get($req, "type") ;
                     $req->said = data_get($req, "transfer.user.fullName");
                     $req->color = $req->type_label == "sell" ? "#ef4444" : "dodgerblue";
                     logger("2", [$req, data_get($req, "userRequest"), data_get($req, "userRequest.fullName"), data_get($req, "transfer"), data_get($req, "transfer.user")]);
