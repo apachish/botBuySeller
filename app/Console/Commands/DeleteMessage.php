e0fb2fccce35cd890028d4ca8011bea65bb9b545
shahriar pahlevansadegh <apachish@gmail.com>
2024-07-09 01:27:24 +0330

    updtae

---
+++ app/Console/Commands/DeleteMessage.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Bot;
+use App\Models\MessageAdmin;
+use App\Services\TelegramServices;
+use Illuminate\Console\Command;
+
+class DeleteMessage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-message';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $message_id = (int)$this->ask('What is  message id?');
+        $bot_user = Bot::where("title", "botUser")->first();
+        if ($bot_user) {
+            $telegram_services = new TelegramServices($bot_user->tokrn);
+            $message = MessageAdmin::where("message_id", $message_id);
+            if ($message)
+                $telegram_services->deleteMessage($message->user_id, $message_id);
+        }
+
+    }
+}
