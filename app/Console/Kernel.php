c2f1c02ac330672dff6c0d0cf9d98167d3ba0809
shahriar pahlevansadegh <apachish@gmail.com>
2024-07-09 00:52:15 +0330

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,14 +25,12 @@ class Kernel extends ConsoleKernel
         $array_time_e = explode(":", data_get($parameter, "end_hours_of_operation.value", "22:00"));
         if($array_time_s) {
             $start_time = Carbon::createFromTime(data_get($array_time_s, 0), data_get($array_time_s, 1), 0)->subMinute("15")->format("H:i");
-            logger("start_time", [$start_time]);
             $schedule->command('app:message-start')->dailyAt($start_time);
         }
         if($array_time_e) {
             $end_time = Carbon::createFromTime(data_get($array_time_e, 0), data_get($array_time_e, 1), 0)->subMinute("15")->format("H:i");
-            logger("end_time", [$end_time]);
 
-            $schedule->command('app:message3')->dailyAt($end_time);
+            $schedule->command('app:message-end')->dailyAt($end_time);
         }
 
     }
