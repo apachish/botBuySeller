d7aad9b6edfeaf8f0422ed83211957718fb304c6
apachish <apachish@gmail.com>
2024-07-23 11:11:48 +0330

    update

--- app/Console/Commands/Message3.php
+++ app/Console/Commands/Message3.php
@@ -42,18 +42,18 @@ class Message3 extends Command
             $bot_user = Bot::where("title", "botUser")->first();
 
             if ($bot_user) {
-                $users = UserTelegram::get();
+                $users = UserTelegram::where("status",1)->get();
                 $telegram_user = new Api($bot_user->token);
                 $telegram_user->sendMessage([
                     'chat_id' => data_get($bot_user, "chanel_id"),
-                    'text' => data_get($message, "value"),
+                    'text' => data_get($message, "message_3.value"),
                 ]);
                 foreach ($users as $user) {
                     try {
                         if (data_get($user, "telegram_id"))
                             $telegram_user->sendMessage([
                                 'chat_id' => data_get($user, "telegram_id"),
-                                'text' => data_get($message, "value"),
+                                'text' => data_get($message, "message_3.value"),
                             ]);
                     } catch (\Exception $exception) {
                         logger("user " . $user->telegram_id . ":" . $exception->getMessage());
