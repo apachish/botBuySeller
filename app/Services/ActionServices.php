c2d698245c222d6565c5f2b359b81b42de82af49
shahriar pahlevansadegh <apachish@gmail.com>
2024-07-25 00:29:19 +0330

    up

--- app/Services/ActionServices.php
+++ app/Services/ActionServices.php
@@ -402,7 +402,11 @@ class ActionServices extends TextServices
         $seller_ids[] = $seller->id;
 
         if ($seller_head)
+        {
             $seller_ids[] = $seller_head->id;
+            $seller_customer_head = $seller_head->customerUser ? $seller_head->customerUsers->pluck("id")->toArray() : [];
+            $seller_customer = array_merge($seller_customer_head, $seller_customer);
+        }
         if ($seller_customer)
             $seller_ids = array_merge($seller_ids, $seller_customer);
 
@@ -412,13 +416,18 @@ class ActionServices extends TextServices
 
         $buyer_ids[] = $buyer->id;
         if ($buyer_head)
+        {
             $buyer_ids[] = $buyer_head->id;
+            $buyer_customer_head = $buyer_head->customerUser ? $buyer_head->customerUsers->pluck("id")->toArray() : [];
+            $buyer_customer = array_merge($buyer_customer_head, $buyer_customer);
+
+        }
         if ($buyer_customer)
             $buyer_ids = array_merge($buyer_ids, $buyer_customer);
         $total_sold_by_seller = 0;
         $total_sold_by_buyer = 0;
-        foreach ($seller_ids as $seller_id) {
-            foreach ($buyer_ids as $buyer_id) {
+        foreach (array_unique($seller_ids) as $seller_id) {
+            foreach (array_unique($buyer_ids) as $buyer_id) {
                 $total_sold_by_seller += DailyRequestTransfer::where('seller_id', $seller_id)
                     ->whereDate("created_at", now())
                     ->where('buyer_id', $buyer_id)->sum('use_day');
