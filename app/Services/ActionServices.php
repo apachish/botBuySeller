c371f3c93492e8eca0ec8cd0eeef4f724db023d7
apachish <apachish@gmail.com>
2025-03-05 17:04:12 +0330

    up

--- app/Services/ActionServices.php
+++ app/Services/ActionServices.php
@@ -947,6 +947,10 @@ class ActionServices extends TextServices
             return true;
         }
 
+        if(env("DISABLE_TOMORROW" ,false) && in_array($this->getType(), $this->list_type_tommarow)){
+            $this->telegram_services->sendMessage($this->getUserId(), "فقط امکان لفظ روز می باشد");
+            return true;
+        }
         if ($this->getDescription()) {
             $ugly_words = UglyWord::get();
             foreach ($ugly_words as $ugly_word) {
